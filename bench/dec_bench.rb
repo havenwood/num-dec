aa6718f21c9a3309e2e98b17ae9d2ce786832936
Shannon Skipper <shannonskipper@gmail.com>
2026-03-29 01:31:56 -0700

    Rework benchmarks

--- bench/dec_bench.rb
+++ bench/dec_bench.rb
@@ -1,49 +1,64 @@
 require "benchmark/ips"
 require "num/dec"
 
-D = Num::Dec
-a = D.from("19.99")
-b = D.from("1.50")
-
 puts "Ruby #{RUBY_VERSION}"
 puts "=" * 60
-puts
+
+d1 = Num::Dec.from("19.99")
+d2 = Num::Dec.from("1.50")
+
+r1 = Rational(1999, 100)
+r2 = Rational(150, 100)
+
+f1 = 19.99
+f2 = 1.50
 
 Benchmark.ips do
   it.config(warmup: 1, time: 3)
 
-  it.report("from(Integer)") { D.from(42) }
-  it.report("from(String)") { D.from("19.99") }
-  it.report("addition") { a + b }
-  it.report("subtraction") { a - b }
-  it.report("multiplication") { a * b }
-  it.report("division") { a / b }
-  it.report("negation") { -a }
-  it.report("<=>") { a <=> b }
-  it.report("zero?") { a.zero? }
-  it.report("abs") { a.abs }
-  it.report("floor") { a.floor }
-  it.report("round(2)") { a.round(2) }
-  it.report("to_s") { a.to_s }
-  it.report("to_f") { a.to_f }
+  it.report("Dec  +") { d1 + d2 }
+  it.report("Rational +") { r1 + r2 }
+  it.report("Float +") { f1 + f2 }
 
   it.compare!
 end
 
 puts
-puts "--- sum 1000 values ---"
-puts
-
-decs = Array.new(1000, a)
-rats = Array.new(1000, Rational(1999, 100))
-floats = Array.new(1000, 19.99)
 
 Benchmark.ips do
   it.config(warmup: 1, time: 3)
 
-  it.report("Dec sum") { decs.sum }
-  it.report("Rational sum") { rats.sum }
-  it.report("Float sum") { floats.sum }
+  it.report("Dec  *") { d1 * d2 }
+  it.report("Rational *") { r1 * r2 }
+  it.report("Float *") { f1 * f2 }
+
+  it.compare!
+end
+
+puts
+
+Benchmark.ips do
+  it.config(warmup: 1, time: 3)
+
+  it.report("Dec  /") { d1 / d2 }
+  it.report("Rational /") { r1 / r2 }
+  it.report("Float /") { f1 / f2 }
+
+  it.compare!
+end
+
+puts
+
+decs = Array.new(1000, d1)
+rats = Array.new(1000, r1)
+ints = Array.new(1000, 1999)
+
+Benchmark.ips do
+  it.config(warmup: 1, time: 3)
+
+  it.report("Dec.sum(1000)") { Num::Dec.sum(decs) }
+  it.report("Integer sum 1000") { ints.sum }
+  it.report("Rational sum 1000") { rats.sum }
 
   it.compare!
 end
